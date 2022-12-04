d3bb59847fa846ad7157e259c914f9970ad3c372
Nicklasson <emma.nicklasson@sogeti.com>
2022-12-04 17:26:40 +0100

    git push

---
+++ day2/Program.cs
@@ -0,0 +1,159 @@
+﻿calculateFirstScore();
+calculateSecondScore();
+
+void calculateFirstScore()
+{
+    try
+    {
+        using (var sr = new StreamReader("../../../input.txt"))
+        {
+            int score = 0;
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine().ToString();
+                if (line != null)
+                {
+                    string elf = line.Split(' ')[0];
+                    string me = line.Split(' ')[1];
+
+                    switch (elf)
+                    {
+                        case "A":
+                            if (me == "X")
+                            {
+                                score = score + 1 + 3;
+                            }
+                            else if (me == "Y")
+                            {
+                                score = score + 2 + 6;
+                            }
+                            else if (me == "Z")
+                            {
+                                score = score + 3;
+                            }
+                            break;
+
+                        case "B":
+                            if (me == "X")
+                            {
+                                score = score + 1;
+                            }
+                            else if (me == "Y")
+                            {
+                                score = score + 2 + 3;
+                            }
+                            else if (me == "Z")
+                            {
+                                score = score + 3 + 6;
+                            }
+                            break;
+
+                        case "C":
+                            if (me == "X")
+                            {
+                                score = score + 1 + 6;
+                            }
+                            else if (me == "Y")
+                            {
+                                score = score + 2;
+                            }
+                            else if (me == "Z")
+                            {
+                                score = score + 3 + 3;
+                            }
+                            break;
+
+                        default:
+                            break;
+
+                    }
+                }
+
+            }
+            Console.WriteLine("Score 1: " + score.ToString());
+        }
+    }
+    catch (IOException e)
+    {
+        Console.WriteLine("The file could not be read:");
+        Console.WriteLine(e.Message);
+    }
+}
+void calculateSecondScore()
+{
+    try
+    {
+        using (var sr = new StreamReader("../../../input.txt"))
+        {
+            int score = 0;
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine().ToString();
+                if (line != null)
+                {
+                    string elf = line.Split(' ')[0];
+                    string me = line.Split(' ')[1];
+
+                    switch (elf)
+                    {
+                        case "A":
+                            if (me == "X")
+                            {
+                                score = score + 3 + 0;
+                            }
+                            else if (me == "Y")
+                            {
+                                score = score + 1 + 3;
+                            }
+                            else if (me == "Z")
+                            {
+                                score = score + 2 + 6;
+                            }
+                            break;
+
+                        case "B":
+                            if (me == "X")
+                            {
+                                score = score + 1 + 0;
+                            }
+                            else if (me == "Y")
+                            {
+                                score = score + 2 + 3;
+                            }
+                            else if (me == "Z")
+                            {
+                                score = score + 3 + 6;
+                            }
+                            break;
+
+                        case "C":
+                            if (me == "X")
+                            {
+                                score = score + 2 + 0;
+                            }
+                            else if (me == "Y")
+                            {
+                                score = score + 3  + 3;
+                            }
+                            else if (me == "Z")
+                            {
+                                score = score + 1 + 6;
+                            }
+                            break;
+
+                        default:
+                            break;
+
+                    }
+                }
+
+            }
+            Console.WriteLine("Score 2: " + score.ToString());
+        }
+    }
+    catch (IOException e)
+    {
+        Console.WriteLine("The file could not be read:");
+        Console.WriteLine(e.Message);
+    }
+}
---
+++ day3/Program.cs
@@ -0,0 +1,99 @@
+﻿part1();
+part2();
+void part1()
+{
+    try
+    {
+        using (var sr = new StreamReader("../../../input.txt"))
+        {
+            int sum = 0;
+            while (!sr.EndOfStream)
+            {
+                var input = sr.ReadLine().ToString();
+                var firstLength = input.Length / 2;
+                var secondLength = input.Length - firstLength;
+
+                string first = input.Substring(0, firstLength);
+                string second = input.Substring(firstLength, secondLength);
+
+                char duplicate = checkForDuplicate(first, second);
+                sum = sum + getPrio(duplicate);
+
+            }
+            Console.WriteLine("Sum1: " + sum);
+        }
+    }
+    catch (IOException e)
+    {
+        Console.WriteLine("The file could not be read:");
+        Console.WriteLine(e.Message);
+    }
+}
+void part2()
+{
+    try
+    {
+        using (var sr = new StreamReader("../../../input.txt"))
+        {
+            int sum = 0;
+
+            while (!sr.EndOfStream)
+            {
+                string[] input = {"","",""};
+                for (int i = 0; i < 3; i++)
+                {
+                    input[i] = sr.ReadLine().ToString();
+                }
+                var duplicate = checkForDuplicate(input[0], input[1]);
+                while (!input[2].Contains(duplicate))
+                {
+                    input[0] = input[0].Replace(duplicate, '-');
+                    input[1] = input[1].Replace(duplicate, '-');
+                    duplicate = checkForDuplicate(input[0], input[1]);
+                }
+                sum = sum + getPrio(duplicate);
+            }
+            Console.WriteLine("Sum2: " + sum );
+        }
+    }
+    catch (IOException e)
+    {
+        Console.WriteLine("The file could not be read:");
+        Console.WriteLine(e.Message);
+    }
+}
+
+char checkForDuplicate(string first, string second)
+{
+    char duplicate = '-';
+    foreach (char c in first)
+    {
+        foreach (char d in second)
+        {
+            if (c == d)
+            {
+                duplicate = d;
+                break;
+            }
+        }
+        if(duplicate!= '-')
+        {
+            break;
+        }
+    }
+    return duplicate;
+}
+
+int getPrio(char c)
+{
+    string alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    for (int i = 0; i < alpha.Length; i++)
+    {
+        if (alpha[i] == c)
+        {
+            return i + 1;
+        }
+    }
+    return 0;
+}
